7a1d55bb2dcf1f0e855b03d5e38b8dcaa7d9b669
hongdat1601 <hongdat1601@gmail.com>
2022-11-03 13:30:33 +0700

    add docker

--- GiuaKy_CNM/obj/Debug/net6.0/GiuaKy_CNM.AssemblyInfo.cs
+++ GiuaKy_CNM/obj/Debug/net6.0/GiuaKy_CNM.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("6ab579fb-1840-4757-9ade-a6ac691d35c0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("GiuaKy_CNM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
